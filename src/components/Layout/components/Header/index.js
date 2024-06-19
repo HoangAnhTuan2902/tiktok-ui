a101abbebc9b3d1e1327f0f012c038c0639b1438
Hoàng Ngọc Anh Tuấn <162965616+HoangAnhTuan2902@users.noreply.github.com>
2024-06-19 14:41:27 +0000

    Update header styles and add search functionality

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,30 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
+import styles from './Header.module.scss';
+import images from '~/assets/images';
 const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/** logo */}
-                {/** search */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Search" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <buton className={cx('loading')}>
+                        <FontAwesomeIcon icon={faSpinner} />
+                    </buton>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
             </div>
         </header>
     );
