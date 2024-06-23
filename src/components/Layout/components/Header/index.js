926ff09bdd61e6be7f7619cc4a6c814afbd06b02
Hoàng Ngọc Anh Tuấn <162965616+HoangAnhTuan2902@users.noreply.github.com>
2024-06-23 14:43:53 +0000

    feat: Add white color variable to GlobalStyles.scss and update Header component with login and registration buttons

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faMagnifyingGlass, faSignIn, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 
+import Button from '~/components/Button';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
@@ -42,16 +43,19 @@ function Header() {
                         <button className={cx('clear')}>
                             <FontAwesomeIcon icon={faCircleXmark} />
                         </button>
-                        <buton className={cx('loading')}>
+                        <button className={cx('loading')}>
                             <FontAwesomeIcon icon={faSpinner} />
-                        </buton>
+                        </button>
 
                         <button className={cx('search-btn')}>
                             <FontAwesomeIcon icon={faMagnifyingGlass} />
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}></div>
+                <div className={cx('actions')}>
+                    <Button text>Log in</Button>
+                    <Button primary>Register</Button>
+                </div>
             </div>
         </header>
     );
